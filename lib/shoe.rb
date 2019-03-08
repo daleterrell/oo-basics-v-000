870a7db3bcdf8f76e38a5cd19bddccb1b8aecd99
Dale Terrell <tragicid@gmail.com>
2019-03-08 17:06:00 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,19 @@
-# Make your shoe class here!
+  class Shoe
+    attr_accessor :brand, :color, :size
+    attr_reader :material, :condition
+  end
+    
+  def initialize(brand)
+    @brand = brand
+  end
+  
+  def brand
+    @brand
+  end
+end
+    
+   
+ 
+ 
+
+  
--- spec/01_book_spec.rb
+++ spec/01_book_spec.rb
@@ -1,38 +1,13 @@
-describe 'Book' do
-  describe '::new' do
-    it 'gets initialized with a title' do
-      expect{Book.new("And Then There Were None")}.to_not raise_error
-    end
+class Book
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
+ 
+  def initialize(title)
+    @title = title
   end
-
-  describe 'properties' do
-    let(:book) { Book.new("And Then There Were None") }
-
-    it 'has a title' do
-      expect(book.title).to eq("And Then There Were None")
-    end
-
-    it 'has an author name' do
-      book.author = "Agatha Christie"
-      expect(book.author).to eq("Agatha Christie")
-    end
-
-    it 'has a page count' do
-      book.page_count = 272
-      expect(book.page_count).to eq(272)
-    end
-
-    it 'has a genre' do
-      book.genre = "Mystery"
-      expect(book.genre).to eq("Mystery")
-    end
+ 
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
   end
-
-  describe '#turn_page' do
-    it 'can turn the page' do
-      expect($stdout).to receive(:puts).with("Flipping the page...wow, you read fast!")
-      book = Book.new("The World According to Garp")
-      book.turn_page 
-    end
-  end
+ 
 end
